57a8bcb850f0efff82bc8bdaba1d04a285885e1f
tmp-sda-1162 <siqi_qian@hotmail.com>
2018-09-14 09:59:39 +0200

    2018-09-14

---
+++ TaskAdministrator/src/com/siqi/taskadmin/TaskDataProcessor.java
@@ -0,0 +1,23 @@
+package com.siqi.taskadmin;
+
+import java.io.File;
+
+import org.json.JSONObject;
+
+public class TaskDataProcessor {
+	private File fileObject;
+	private File dirObject;
+	private JSONObject json;
+	
+	
+	
+	public void reader() {
+		
+	}
+	
+	public void writer() {
+		
+	}
+}
+
+
